08a985e227c2c5f0b4674d11c4db1332211c87df
Sooyoung Yoon <ayoooo240@gmail.com>
2022-05-19 03:29:58 +0900

    ✨ 기본 시드 설정

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
-
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
