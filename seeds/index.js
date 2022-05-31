e682d0464ae5a121df31ad3a8969ca7c567771f1
Sooyoung Yoon <ayoooo240@gmail.com>
2022-05-31 20:10:02 +0900

    ✨ author 정보 추가한 새 campground 데이터 생성

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '6295b8b84f34f1e3308032c6',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
