4c9bdf37d6265f762396ad3041a53834310db855
Sooyoung Yoon <ayoooo240@gmail.com>
2022-06-02 05:18:41 +0900

    ✨ campground 편집 페이지도 사진 업로드 기능 추가

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -14,7 +14,6 @@ module.exports.createCampground = async (req, res, next) => {
   campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
   campground.author = req.user._id;
   await campground.save();
-  console.log(campground);
   req.flash('success', 'Successfully made a new campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
@@ -49,6 +48,9 @@ module.exports.updateCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+  const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  await campground.save();
   req.flash('success', 'Successfully updated campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
